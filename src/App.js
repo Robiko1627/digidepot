f813027e2f2ad6f6b998bc19a08544d29bbcdce3
Robiko1627 <robinkioko27@gmail.com>
2025-03-21 13:05:22 +0300

    your commit message

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes,Link} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Getappliance from './components/Getappliance';
 
@@ -8,10 +8,22 @@ function App() {
     <Router>
       <div className="App">
       <header className="App-header">
-        <h1 className='text-success'>Digi</h1><h1 className='text-danger'>Depot</h1><h1> - Home of Everything Tech</h1>
+        <p><b><span className='text-success'>Digi</span> <span className='text-warning'>Depot</span></b> <h9>- Home of Everything Tech</h9></p>
       </header>
+      <nav>
+          <Link to={'/'} className='Links'>Get Appliance</Link>
+          <Link to={'/signup'} className='Links'>Sign up</Link>
+          <Link to={'/signin'} className='Links'>Sign in</Link>
+          <Link to={'/addappliance'} className='Links'>Add Appliance</Link>
+        </nav>
+
       <Routes>
-      <Route path ='/getappliance' element = {<Getappliance/>}/>
+        <Route path ='/' element = {<Getappliance/>}/>     
+        <Route path ='/addappliance' element = {<Addappliance/>}/>
+        <Route path ='/signup' element = {<Signup/>}/>
+        <Route path ='/signin' element = {<Signin/>}/>
+        <Route path='/mpesapayment' element={<Mpesapayment/>}/>
+
       </Routes>
     </div>
     </Router>
---
+++ src/components/Addappliance.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Addappliance = () => {
+  return (
+    <div>
+        <h2>Welcome to the Add Appliance page</h2>
+      
+    </div>
+  )
+}
+
+export default Addappliance
